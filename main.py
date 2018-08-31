fd3f6feee2bb3c2b03d486af653bee7b4814e5b4
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2018-08-31 14:48:29 +0900

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, WWWWWWWrld!'
+    return 'Hellod!'
 
 if __name__ == "__main__":
     app.run(port=8000, host='0.0.0.0')
