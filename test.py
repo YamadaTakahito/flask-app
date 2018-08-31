5033884c4f67aff4104fc417e15e8ce96946ff52
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2018-08-31 19:23:39 +0900

    test

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 import unittest
 
-import requests
-
-
 class TestPing(unittest.TestCase):
     def test_ping(self):
         self.assertEqual(requests.get('http://localhost:8000/ping').content, b'pong')
